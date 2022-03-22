c7ba0c6310e5677bfad0e634cfda9d8b226ff10a
Zaman Sheikh <zaman6545@gmail.com>
2022-03-22 19:08:53 +0600

    22/03.2022

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        boolean isRunning = true;
+        while (isRunning) {
+            System.out.println("     _        __  __           _ _ _     _   _                 _ _        _ ");
+            System.out.println("  __| | __ _ / _|/ _| ___   __| (_) |   | | | | ___  ___ _ __ (_) |_ __ _| |");
+            System.out.println(" / _` |/ _` | |_| |_ / _ \\ / _` | | |   | |_| |/ _ \\/ __| '_ \\| | __/ _` | |");
+            System.out.println("| (_| | (_| |  _|  _| (_) | (_| | | |   |  _  | (_) \\__ \\ |_) | | || (_| | |");
+            System.out.println(" \\__,_|\\__,_|_| |_|  \\___/ \\__,_|_|_|___|_| |_|\\___/|___/ .__/|_|\\__\\__,_|_|");
+            System.out.println("                                   |_____|              |_|                 ");
+            System.out.println("                                                                          ");
+
+            Scanner s = new Scanner(System.in);
+            int choice = 9;
+            System.out.println("Welcome to the main menu!");
+            System.out.println("Please select an option:");
+            System.out.println("2. Login to an existing account");
+            System.out.println("0. Exit");
+            choice = s.nextInt();
+            if (choice == 1) {
+                System.out.println("Say hi from op 1");
+                continue;
+            }
+            if (choice == 0){
+                isRunning = false;
+            } 
+
+        }
+
+
+    }
+}
